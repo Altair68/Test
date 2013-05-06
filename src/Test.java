805cf4d1f20adba174a8fddddc7b7d465a07064e
Manu <manuel.bareiss92@gmail.com>
2013-05-06 18:30:55 +0200

    Test

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Manu
+ * Date: 06.05.13
+ * Time: 18:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+	//Das ist ein Test
+}
